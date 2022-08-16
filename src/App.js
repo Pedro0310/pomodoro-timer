fee96c388370310ed14b145f1522917ecaa6d7e2
Pedro0310 <ruizpedro567@gmail.com>
2022-08-16 18:27:13 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useContext } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import Button from "./components/Button";
+import CountdownAnimation from "./components/CountdownAnimation";
+import SetPomodoro from "./components/SetPomodoro";
+import { SettingContext } from "./context/SettingsContext";
 
 function App() {
+  const {
+    pomodoro,
+    executing,
+    setCurrentTimer,
+    SettingBtn,
+    children,
+    startAnimate,
+    startTimer,
+    pauseTimer,
+    updateExecute,
+  } = useContext(SettingContext);
+
+  useEffect(() => updateExecute(executing), [executing, startAnimate]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Pomodoro</h1>
+      <small>Be productive the right way.</small>
+      {pomodoro == 0 ? (
+        <SetPomodoro />
+      ) : (
+        <>
+          <ul className="labels">
+            <li>
+              <Button
+                title="Work"
+                activeClass={executing.active === "work" && "active-label"}
+                _callback={() => setCurrentTimer("work")}
+              />
+            </li>
+            <li>
+              <Button
+                title="Short Break"
+                activeClass={executing.active === "short" && "active-label"}
+                _callback={() => setCurrentTimer("short")}
+              />
+            </li>
+            <li>
+              <Button
+                title="Long Break"
+                activeClass={executing.active === "long" && "active-label"}
+                _callback={() => setCurrentTimer("long")}
+              />
+            </li>
+          </ul>
+          <Button title="Settings" _callback={SettingBtn} />
+          <div className="time-container">
+            <div className="time-wrapper">
+              <CountdownAnimation
+                key={pomodoro}
+                timer={pomodoro}
+                animate={startAnimate}
+              >
+                {children}
+              </CountdownAnimation>
+            </div>
+          </div>
+
+          <div className="button-wrapper">
+            <Button
+              title="Start"
+              className={!startAnimate && "active"}
+              _callback={startTimer}
+            />
+            <Button
+              title="Pause"
+              className={startAnimate && "active"}
+              _callback={pauseTimer}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/context/SettingsContext.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const SettingContext = createContext();
+
+const SettingsContextProvider = (props) => {
+  const [pomodoro, setPomodoro] = useState(0);
+  const [executing, setExecuting] = useState({});
+  const [startAnimate, setStartAnimate] = useState(false);
+
+  function startTimer() {
+    setStartAnimate(true);
+  }
+
+  function stopTimer() {
+    setStartAnimate(false);
+  }
+
+  function pauseTimer() {
+    setStartAnimate(false);
+  }
+
+  const SettingBtn = () => {
+    setExecuting({});
+    setPomodoro(0);
+  };
+
+  function setCurrentTimer(active_state) {
+    updateExecute({
+      ...executing,
+      active: active_state,
+    });
+    setTimerTime(executing);
+  }
+
+  const updateExecute = (updatedSettings) => {
+    setExecuting(updatedSettings);
+    setTimerTime(updatedSettings);
+  };
+
+  const setTimerTime = (evaluate) => {
+    switch (evaluate.active) {
+      case "work":
+        setPomodoro(evaluate.work);
+        break;
+
+      case "short":
+        setPomodoro(evaluate.short);
+        break;
+
+      case "long":
+        setPomodoro(evaluate.long);
+        break;
+
+      default:
+        setPomodoro(0);
+        break;
+    }
+  };
+
+  const children = ({ remainingTime }) => {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+
+    return `${minutes}:${seconds}`;
+  };
+
+  return (
+    <SettingContext.Provider value={{ stopTimer, 
+    updateExecute,
+    pomodoro,
+    executing,
+    startAnimate,
+    startTimer,
+    pauseTimer,
+    stopTimer,
+    SettingBtn,
+    setCurrentTimer,
+    updateExecute,
+    children
+    }}>
+      {props.children}
+    </SettingContext.Provider>
+  );
+};
+
+export default SettingsContextProvider;
